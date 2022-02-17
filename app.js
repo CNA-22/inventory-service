5017f90c89e65fdabc7075b91a1e84e413d35932
Johannes Vähäkangas <vahakanj@arcada.fi>
2022-02-17 11:00:46 +0200

    authoriztion

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const db = require('./products')
 const app = express()
 const cors = require("cors")
 const PORT = process.env.PORT || 3000
+const authorization = require('./middleware/authorize')
 
 app.use(
   cors({
@@ -21,12 +22,13 @@ app.get('/', (req, res) => {
   res.json({ all_data: '/products', by_sku: '/products/:pid'})
 })
 
-app.get('/products', db.getProducts)
-app.get('/products/:pid', db.getProductByPid)
-app.post('/products', db.createProduct)
-app.patch('/products/edit/:pid', db.editProduct)
-app.patch('/products/reduce/:pid', db.reduceProduct)
-app.delete('/products/:pid', db.deleteProduct)
+app.get('/products',authorization, db.getProducts)
+app.get('/products/:pid', authorization, db.getProductByPid)
+app.post('/products', authorization,db.createProduct)
+app.patch('/products/edit/:pid', authorization,db.editProduct)
+app.patch('/products/reduce/:pid',authorization ,db.reduceProduct)
+app.delete('/products/:pid', authorization ,db.deleteProduct)
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}.`)
--- products.js
+++ products.js
@@ -71,6 +71,8 @@ const deleteProduct = (req, res) => {
     })
 }
 
+
+
 module.exports = {
     getProducts,
     getProductByPid,
