7e205d4203cdc748d245ee6685522810b2c14e45
lindreas <andreas.lindgren@arcada.fi>
2022-02-21 08:44:17 +0200

    Added api docs with Swagger ui

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const swaggerDocument = require('./swagger.json')
+const swaggerUi = require('swagger-ui-express')
 const bodyParser = require('body-parser')
 const db = require('./products')
 const app = express()
 const cors = require("cors")
 const PORT = process.env.PORT || 3000
 const authorization = require('./middleware/authorize')
+
+app.use("/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+)
 
 app.use(
   cors({
@@ -20,7 +27,7 @@ app.use(
 )
 
 app.get('/', (req, res) => {
-  res.json({ all_data: '/products', by_pid: '/products/:pid'})
+  res.json({ api_documentation: '/api-docs'})
 })
 
 app.get('/products',authorization, db.getProducts)
@@ -28,7 +35,7 @@ app.get('/products/:pid', authorization, db.getProductByPid)
 app.post('/products', authorization,db.createProduct)
 app.patch('/products/edit/:pid', authorization,db.editProduct)
 app.patch('/products/reduce/:pid',authorization ,db.reduceProduct)
-app.delete('/products/:pid', authorization ,db.deleteProduct)
+app.delete('/products/delete/:pid', authorization ,db.deleteProduct)
 
 
 app.listen(PORT, () => {
